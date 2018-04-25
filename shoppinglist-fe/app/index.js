d346be62d8f5dae73b78732bd489a9aca76cbfa8
Administrator <antoine.wils@gmail.com>
2018-04-25 23:07:41 +0200

    handle shomping list proposed item in javascript

--- shoppinglist-fe/app/index.js
+++ shoppinglist-fe/app/index.js
@@ -9,3 +9,33 @@ if (typeof document !== 'undefined') {
 } else {
     console.log(greeting);
 }
+
+
+var greeter = require('./greeter');
+
+var greeting = greeter.greet();
+
+if (typeof document !== 'undefined') {
+    var apiEndpoint = 'http://localhost:8090/proposedItems';
+    var table = document.createElement('table');
+
+    fetch(apiEndpoint).then(function(response) {
+        return response.json();
+    }).then(function(obj) {
+        let html = "";
+        obj.forEach((item) => html +=`
+            <tr>
+                <td>${item.category}</td>
+                <td>${item.type}</td>
+                <td>${item.brand}</td>
+                <td>${item.product}</td>
+            </tr>`);
+        table.innerHTML = html;
+        document.body.appendChild(table);
+    }).catch(function(err) {
+        table.innerHTML = '<tr><td>oh no…</td></tr>';
+        document.body.appendChild(table);
+    });
+} else {
+    console.log(greeting);
+}
