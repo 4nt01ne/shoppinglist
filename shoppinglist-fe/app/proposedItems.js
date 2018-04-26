8bdc21d26b6229dedba426140c9d244cd1f87e91
Administrator <antoine.wils@gmail.com>
2018-04-27 01:55:58 +0200

    proposed shopping list as an html list that can be collapsed + playing with responsive css3

--- shoppinglist-fe/app/proposedItems.js
+++ shoppinglist-fe/app/proposedItems.js
@@ -3,38 +3,42 @@ var greeter = require('./greeter');
 var greeting = greeter.greet();
 
 if (typeof document !== 'undefined') {
-    var el = document.createElement('h1');
-    el.innerHTML = greeting;
-    document.body.appendChild(el);
-} else {
-    console.log(greeting);
-}
-
-
-var greeter = require('./greeter');
-
-var greeting = greeter.greet();
-
-if (typeof document !== 'undefined') {
-    var apiEndpoint = 'http://localhost:8090/proposedItems';
-    var table = document.createElement('table');
+    let apiEndpoint = 'http://localhost:8090/proposedItems';
+    //let list = document.getElementsByClassName('css-treeview');
+    let list = document.createElement('div');
+    list.classList.add("css-treeview");
 
     fetch(apiEndpoint).then(function(response) {
         return response.json();
     }).then(function(obj) {
-        let html = "";
-        obj.forEach((item) => html +=`
-            <tr>
-                <td>${item.category}</td>
-                <td>${item.type}</td>
-                <td>${item.brand}</td>
-                <td>${item.product}</td>
-            </tr>`);
-        table.innerHTML = html;
-        document.body.appendChild(table);
+        let items = new Map();
+        obj.forEach((item) => {
+            items.set(item.category, items.get(item.category) || []);
+            items.get(item.category).push(item);
+        });
+        let html = '    <ul>';
+        let i = 0;
+        items.forEach((value,key) => {
+            //category
+            html +=`
+        <li><input type="checkbox" id="item-${i}" /><label for="item-${i}">${key}</label>
+            <ul>`;
+            //category items
+            items.get(key).forEach((item) => {
+                html += `
+                <li>${item.type}, ${item.brand}, ${item.product}</li>`;
+            });
+            html +=`
+            </ul>
+        </li>`;
+            i++;
+        });
+        html += `
+    </ul>`;
+        list.innerHTML = html;
+        document.body.appendChild(list);
     }).catch(function(err) {
-        table.innerHTML = '<tr><td>oh no…</td></tr>';
-        document.body.appendChild(table);
+        list.innerHTML = '<p>oh no…</p>';
     });
 } else {
     console.log(greeting);
