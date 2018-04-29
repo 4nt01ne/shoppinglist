f097b9f22676bde1ad416aa63db77a1099432528
Administrator <antoine.wils@gmail.com>
2018-04-29 02:51:03 +0200

    list expand/collapse icon, indent category elements and gradient backgrounds

--- shoppinglist-fe/app/proposedItems.js
+++ shoppinglist-fe/app/proposedItems.js
@@ -1,45 +1,63 @@
-var greeter = require('./greeter');
+let colorHelp = require('./color');
+let apiEndpoint = 'http://localhost:8090/proposedItems';
+let list = document.createElement('div');
+list.classList.add("css-treeview");
 
-var greeting = greeter.greet();
-
-if (typeof document !== 'undefined') {
-    let apiEndpoint = 'http://localhost:8090/proposedItems';
-    //let list = document.getElementsByClassName('css-treeview');
-    let list = document.createElement('div');
-    list.classList.add("css-treeview");
-
-    fetch(apiEndpoint).then(function(response) {
-        return response.json();
-    }).then(function(obj) {
-        let items = new Map();
-        obj.forEach((item) => {
-            items.set(item.category, items.get(item.category) || []);
-            items.get(item.category).push(item);
-        });
+fetch(apiEndpoint).then(function (response) {
+    return response.json();
+}).then(function (obj) {
+    let items = new Map();
+    obj.forEach((item) => {
+        items.set(item.category, items.get(item.category) || []);
+        items.get(item.category).push(item);
+    });
+    if (typeof document !== 'undefined') {
         let html = '    <ul>';
         let i = 0;
-        items.forEach((value,key) => {
+        let gradientScaleItems = 1/ items.size;
+        items.forEach((value, key) => {
             //category
-            html +=`
-        <li><input type="checkbox" id="item-${i}" /><label for="item-${i}">${key}</label>
+            html += `
+        <li><input type="checkbox" id="item-${i}" /><label for="item-${i}" style="background-color: ${colorHelp.colorLuminance("#7d6608",gradientScaleItems)};">${key}</label>
             <ul>`;
+            let gradientScaleSubItems = 1/ items.get(key).length;
             //category items
             items.get(key).forEach((item) => {
                 html += `
-                <li>${item.type}, ${item.brand}, ${item.product}</li>`;
+                <li class="item" style="background-color: ${colorHelp.colorLuminance("#154360",gradientScaleSubItems)};">${item.type}, ${item.brand}, ${item.product}</li>`;
+                gradientScaleSubItems += gradientScaleSubItems;
             });
-            html +=`
+            html += `
+
             </ul>
         </li>`;
             i++;
+            gradientScaleItems += gradientScaleItems;
         });
         html += `
     </ul>`;
         list.innerHTML = html;
         document.body.appendChild(list);
-    }).catch(function(err) {
-        list.innerHTML = '<p>oh no…</p>';
-    });
-} else {
-    console.log(greeting);
-}
+    } else {
+        let message = '';
+        items.forEach((value, key) => {
+            //category
+            message += `
+        ${key}`;
+            //category items
+            items.get(key).forEach((item) => {
+                message += `
+                ${item.type}, ${item.brand}, ${item.product}`;
+            });
+            i++;
+        });
+        console.log(message);
+    }
+}).catch(function (err) {
+    if (typeof document !== 'undefined') {
+        list.innerHTML =  `<p>oh no… ${err}</p>`;
+        document.body.appendChild(list);
+    } else {
+        console.log('oh no... ' + err);
+    }
+});
